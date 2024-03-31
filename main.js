cbbc4d50b8a081fa88b4ea187fb2637c0cbf897e
abdullohnajmiddinov <abdullohnajmiddinov45@gmail.com>
2024-03-31 18:11:01 +0300

    done

---
+++ main.js
@@ -0,0 +1,17 @@
+let meva = ["Olma", "Banan", "Gilos", "shoptoli"];
+
+console.log(meva);
+alert("Boshlanishida Arrayning uzunligi: " + meva.length);
+
+let con = confirm("Sizning arrayingizdan malumotni olib tashlamoqchimisiz?");
+
+if (con === true) {
+    meva.pop()
+alert('Arrayning uzunligi ' + meva.length)
+console.log(meva);
+} else {
+    alert('Arrayning uzunligi ' + meva.length)
+    console.log(meva);
+}
+
+
